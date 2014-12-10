f6e8eba50cb4929d1554beb2151bb6662194230c
ratzuka <niklaschristensen@hotmail.dk>
2014-12-10 01:01:12 +0100

    halvt færdig edit order
    kan nu tilfæje ordre, ikke fjærne eller vis mere end 2 endu

--- ProductionLtd/ProductionLtd/ChangeOrder.xaml.cs
+++ ProductionLtd/ProductionLtd/ChangeOrder.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace ProductionLtd
 {
@@ -22,6 +24,43 @@ namespace ProductionLtd
         public ChangeOrder()
         {
             InitializeComponent();
+            refreshDisplayData();
+        }
+
+        private void refreshDisplayData()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                {
+                    SqlConnection conn = new SqlConnection(
+                        "Server=ealdb1.eal.local;" +
+                    "Database=EJL09_DB;" +
+                    "User Id=ejl09_usr;" +
+                    "Password=Baz1nga9;");
+                    try
+                    {
+                        conn.Open();
+                        SqlCommand cmd = new SqlCommand("PrintAllOrders", conn);
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.Add(new SqlParameter("ID", i));
+                        SqlDataReader rdr = cmd.ExecuteReader();
+                        while (rdr.HasRows && rdr.Read())
+                        {
+                            OrderListePrintTextBox.Text += rdr["ID"].ToString() + ": " + rdr["Name"].ToString() + ", " + rdr["DeadLine"].ToString() + rdr["Pris"].ToString() + ", " + rdr["RistType"].ToString() + ", " + rdr["Antal"].ToString() + ", " + "\n";
+                        }
+                    }
+                    catch (Exception k)
+                    {
+                        string kk = k.ToString();
+                        System.Windows.MessageBox.Show(kk);
+                    }
+                    finally
+                    {
+                        conn.Close();
+                        conn.Dispose();
+                    }
+                }
+            }
         }
     }
 }
--- ProductionLtd/ProductionLtd/MakeOrder.xaml.cs
+++ ProductionLtd/ProductionLtd/MakeOrder.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace ProductionLtd
 {
@@ -30,5 +32,61 @@ namespace ProductionLtd
             Admin_Site AS = new Admin_Site();
             AS.ShowDialog();
         }
+
+        private void AddOrderButtom_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(
+                "Server=ealdb1.eal.local;" +
+            "Database=EJL09_DB;" +
+            "User Id=ejl09_usr;" +
+            "Password=Baz1nga9;");
+            try
+            {
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand("AddOrder", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.Add(new SqlParameter("@Name", OrderTextBox.Text));
+                cmd.Parameters.Add(new SqlParameter("@DeadLine", SlutDatoTextBox.Text));
+                cmd.Parameters.Add(new SqlParameter("@Pris", PrisTextBox.Text));
+                cmd.Parameters.Add(new SqlParameter("@RistType", ValgtRistTextBox.Text));
+                cmd.Parameters.Add(new SqlParameter("@Antal", AntalTextBox.Text));
+
+
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception k)
+            {
+                string kk = k.ToString();
+                System.Windows.MessageBox.Show(kk);
+            }
+            finally
+            {
+                conn.Close();
+                conn.Dispose();
+            }
+        }
+
+        private void Type1Button_Click(object sender, RoutedEventArgs e)
+        {
+            ValgtRistTextBox.Text = "Rist type 1";
+        }
+
+        private void Type2Button_Click(object sender, RoutedEventArgs e)
+        {
+            ValgtRistTextBox.Text = "Rist type 2";
+        }
+
+        private void Type3Button_Click(object sender, RoutedEventArgs e)
+        {
+            ValgtRistTextBox.Text = "Rist type 3";
+        }
+
+        private void KundebestemtButton_Click(object sender, RoutedEventArgs e)
+        {
+            ValgtRistTextBox.Text = "Special bestilt";
+        }
+
     }
 }
